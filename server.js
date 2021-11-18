0b61ffe6c0f652d79707a819fe292c8e7700e2f1
SNValadez <snvaladez13@gmail.com>
2021-11-17 19:53:53 -0800

    require passport on server

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 const express = require("express");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
+
+const passport = require("./config/passport");
+
+const PORT = process.env.PORT || 3001;
