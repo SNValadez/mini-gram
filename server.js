00cfff1cbf451bf077d62458aed688287772eb9b
SNValadez <snvaladez13@gmail.com>
2021-11-17 20:21:23 -0800

    app.engine and app.set

--- server.js
+++ server.js
@@ -11,3 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
