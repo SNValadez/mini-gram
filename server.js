1835b580f4f448646320a847ff3d29472dc11ce8
SNValadez <snvaladez13@gmail.com>
2021-11-17 20:15:43 -0800

    require app.use

--- server.js
+++ server.js
@@ -3,5 +3,9 @@ const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
 
 const passport = require("./config/passport");
+const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
