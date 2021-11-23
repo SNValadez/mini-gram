0c19d68e0218c991977a08c2f43005fa1ad412d9
swillrodt <sumnerwillrodt@gmail.com>
2021-11-22 19:17:37 -0800

    setup handlebars

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,24 @@ module.exports = {
   darkMode: false,
   theme {
     extend: {},
+    colors: {
+      teal: {
+        light: '#85b9af',
+        default: '#1a9988',
+        dark: '#0f594c'
+      },
+      purple: {
+        light: '#b4a8d2',
+        default: '#8e7cc3',
+        dark: '#4d3e6d'
+      },
+      orange: {
+        light: '#f2976c',
+        default: '#eb5600',
+        dark: '#a53a19'
+      },
+      white: '#f1f1f1'
+    }
   },
   variants: {},
   plugins []
