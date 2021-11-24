df31f5d9b6880ffdf4d4b9cdc18c0428bd54f7ca
swillrodt <sumnerwillrodt@gmail.com>
2021-11-23 23:34:03 -0800

    add dashboard page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 module.exports = {
   purge: [],
   darkMode: false,
-  theme {
+  theme: {
     extend: {},
-    colors: {
-      teal: {
-        light: '#85b9af',
-        default: '#1a9988',
-        dark: '#0f594c'
-      },
-      purple: {
-        light: '#b4a8d2',
-        default: '#8e7cc3',
-        dark: '#4d3e6d'
-      },
-      orange: {
-        light: '#f2976c',
-        default: '#eb5600',
-        dark: '#a53a19'
-      },
-      white: '#f1f1f1'
-    }
+    // colors: {
+    //   teal: {
+    //     light: '#85b9af',
+    //     default: '#1a9988',
+    //     dark: '#0f594c'
+    //   },
+    //   purple: {
+    //     light: '#b4a8d2',
+    //     default: '#8e7cc3',
+    //     dark: '#4d3e6d'
+    //   },
+    //   orange: {
+    //     light: '#f2976c',
+    //     default: '#eb5600',
+    //     dark: '#a53a19'
+    //   },
+    //   white: '#f1f1f1'
+    // }
   },
   variants: {},
-  plugins []
+  plugins: []
 }
